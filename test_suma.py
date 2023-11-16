c16a6f620bf06d0828da0d61bd7b467490de8dd6
Alberto de la Oliva <albertodelaoliva1990@gmail.com>
2023-11-16 12:44:54 +0100

    Arreglado eso me pasa por copia lo del profesor que se olvida poner el codigo y luego dice que funciona

--- test_suma.py
+++ test_suma.py
@@ -3,9 +3,9 @@ from suma import sumar
 
 class TestSumar(unittest.TestCase):
     def test_sumar(self):
-        self.assertEqual((3, 2), 5)
-        self.assertEqual((-1, 1), 0)
-        self.assertEqual((-1, -1), -2)
+        self.assertEqual(sumar(3, 2), 5)
+        self.assertEqual(sumar(-1, 1), 0)
+        self.assertEqual(sumar(-1, -1), -2)
 
 if __name__ == "__main__":
     unittest.main()
